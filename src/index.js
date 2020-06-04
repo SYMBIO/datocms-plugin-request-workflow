9f24c8e2d29ca61cd1dcbcdaf275d950f5f3467b
Martin Cajthaml <martin.cajthaml@symbio.agency>
2020-06-04 03:36:58 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,32 +7,34 @@ window.DatoCmsPlugin.init((plugin) => {
 
   const container = document.createElement('div');
   container.classList.add('container');
-  container.textContent = `Stav: ${stage}`;
+  container.textContent = `${stage}`;
 
-  const approveButton = document.createElement('button');
-  approveButton.classList.add('approveButton');
-  approveButton.textContent = 'Schválit';
-  approveButton.addEventListener('click', () => {
-    plugin.saveCurrentItem()
-      .then(() => {
-        window.location.href = `https://wineofczechrepublic.symbio.agency/api/approveRequest?id=${plugin.itemId}&type=${plugin.itemType.id}`;
-      })
-      .catch(() => plugin.alert('Nepodařilo se schválit položku'));
-  });
+  if (stage === 'Nový') {
+    const approveButton = document.createElement('button');
+    approveButton.classList.add('approveButton');
+    approveButton.textContent = 'Schválit';
+    approveButton.addEventListener('click', () => {
+      plugin.saveCurrentItem()
+        .then(() => {
+          window.location.href = `https://wineofczechrepublic.symbio.agency/api/approveRequest?id=${plugin.itemId}&type=${plugin.itemType.id}`;
+        })
+        .catch(() => plugin.alert('Nepodařilo se schválit položku'));
+    });
 
-  const denyButton = document.createElement('button');
-  denyButton.classList.add('denyButton');
-  denyButton.textContent = 'Zamítnout';
-  denyButton.addEventListener('click', () => {
-    plugin.saveCurrentItem()
-      .then(() => {
-        window.location.href = `https://wineofczechrepublic.symbio.agency/api/denyRequest?id=${plugin.itemId}&type=${plugin.itemType.id}`;
-      })
-      .catch(() => plugin.alert('Nepodařilo se zamítnout položku'));
-  });
+    const denyButton = document.createElement('button');
+    denyButton.classList.add('denyButton');
+    denyButton.textContent = 'Zamítnout';
+    denyButton.addEventListener('click', () => {
+      plugin.saveCurrentItem()
+        .then(() => {
+          window.location.href = `https://wineofczechrepublic.symbio.agency/api/denyRequest?id=${plugin.itemId}&type=${plugin.itemType.id}`;
+        })
+        .catch(() => plugin.alert('Nepodařilo se zamítnout položku'));
+    });
 
-  container.appendChild(approveButton);
-  container.appendChild(denyButton);
+    container.appendChild(approveButton);
+    container.appendChild(denyButton);
+  }
 
   document.body.appendChild(container);
 });
