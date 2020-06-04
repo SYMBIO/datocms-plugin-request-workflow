c458f746a1235c771cc34e2ff3cf23c8bb5eda1e
Martin Cajthaml <martin.cajthaml@symbio.agency>
2020-06-04 03:26:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,5 +9,23 @@ window.DatoCmsPlugin.init((plugin) => {
   container.classList.add('container');
   container.textContent = `Stav: ${stage}`;
 
+  const approveButton = document.createElement('button');
+  approveButton.classList.add('approveButton');
+  approveButton.textContent = 'Schválit';
+  approveButton.addEventListener('click', async () => {
+    plugin.saveCurrentItem()
+      .then(() => {
+        window.location.href = `https://wineofczechrepublic.symbio.agency/api/approveRequest?id=${plugin.itemId}&type=${plugin.itemType.id}`;
+      })
+      .catch(() => console.log('Could not save this record'));
+  });
+
+  const denyButton = document.createElement('button');
+  denyButton.classList.add('denyButton');
+  denyButton.textContent = 'Zamítnout';
+
+  container.appendChild(approveButton);
+  container.appendChild(denyButton);
+
   document.body.appendChild(container);
 });
